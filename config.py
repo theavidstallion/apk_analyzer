7f5facd7dc62c6825dca288466668a83ed9e76e8
Farooq Ahmed <89699180+theavidstallion@users.noreply.github.com>
2025-01-20 06:03:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'farooqj072@gmail.com'  # Enter your email here
+    MAIL_USERNAME = 'raventechgeeks@gmail.com'  # Enter your email here
     MAIL_PASSWORD = 'rzdx ldtl uyzf sgvo'  # Enter your password here
 
     # Google OAuth Config (if you want to use OAuth)
-    GOOGLE_CLIENT_ID = 'xxxx'
-    GOOGLE_CLIENT_SECRET = 'xxxx'
+    GOOGLE_CLIENT_ID = '89689369343-r0fq61v09s5q3e3dbnajvm4abha0jhod.apps.googleusercontent.com'
+    GOOGLE_CLIENT_SECRET = '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'
     GOOGLE_DISCOVERY_URL = 'https://accounts.google.com/.well-known/openid-configuration'
